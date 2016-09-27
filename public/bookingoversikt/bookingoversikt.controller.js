a052645220a1eb93c58b32eff129301a52acd11a
Joachim Eivindsen <Joachim Eivindsen>
2016-09-27 12:00:59 +0200

    Laget filtreringsknapper, muliggjort sortering av database utifra dato, lagt ved normalize.css og fikset mindre feil.

--- public/bookingoversikt/bookingoversikt.controller.js
+++ public/bookingoversikt/bookingoversikt.controller.js
@@ -9,9 +9,41 @@
 
     FirebaseService.getBookings(function(response) {
       $scope.$apply(function() {
-        $scope.bookings = response.val();
+        var bookings = response.val();
+        var sorterteBookings = new Object();
+          for (var bookingID in bookings) {
+            if (bookings.hasOwnProperty(bookingID)) {
+              if(!(bookings[bookingID].dato in sorterteBookings)) {
+                sorterteBookings[bookings[bookingID].dato] = new Object();
+                sorterteBookings[bookings[bookingID].dato][bookingID] = bookings[bookingID];
+              } else {
+                sorterteBookings[bookings[bookingID].dato][bookingID] = bookings[bookingID];
+              }
+            }
+        }
+        console.log(sorterteBookings);
+        $scope.sorterteBookings = sorterteBookings;
+        $scope.valgtStatus = "velg";
+        $scope.valgtScene = "velg";
       });
     });
 
+    $scope.filterFunction = function(bookings) {
+      var filtrertList = new Object();
+      for (var dato in bookings) {
+        var filtrertDato = new Object;
+        angular.forEach(bookings[dato], function(value,key) {
+          if((value.status == $scope.valgtStatus || $scope.valgtStatus == "velg") && (value.scene == $scope.valgtScene || $scope.valgtScene == "velg")) {
+            filtrertDato[key] = value;
+          }
+        });
+        if(!($.isEmptyObject(filtrertDato))) {
+          filtrertList[dato] = filtrertDato;
+        }
+      }
+      return filtrertList;
+    }
+
   }
+
 })();
