5210a9b1f2065f9d59bae512261ce265eb1f9e96
Torkil Svanes Vatne <torkilsv@stud.ntnu.no>
2016-10-04 17:27:44 +0200

    Lagt til søkefunksjon for 'ny booking'. Lagt til modaler for hvert søkeobjekt. Lagt til CSS i main.css for å fikse på modal.

--- public/new-booking/new-booking.controller.js
+++ public/new-booking/new-booking.controller.js
@@ -4,8 +4,42 @@
   angular.module("angularAuth")
   .controller("newBookingCtrl", newBookingCtrl);
 
-  newBookingCtrl.$inject = ["$scope"];
-  function newBookingCtrl($scope) {
+  newBookingCtrl.$inject = ["$scope", "SpotifyService"];
+  function newBookingCtrl($scope, SpotifyService) {
 
+
+    $scope.searchForArtist = function (searchText) {
+      SpotifyService.searchArtist(searchText, 'artist').then(function (data) {
+        console.log(data);
+        $scope.searchResult = data.artists.items;
+      });
+    };
+
+    $scope.sendBooking = function (booking) {
+      console.log(booking);
+      var newPostKey = firebase.database().ref().child('bookinger').push().key;
+      firebase.database().ref().child('bookinger').child(newPostKey).set({
+          artist : booking.artist,
+          billettpris : booking.billettpris,
+          dato : booking.dato.getYear() + "-" + booking.dato.getMonth() + "-" + booking.dato.getDate(),
+          kostnad : booking.kostnad,
+          scene : booking.scene,
+          bookingansvarlig : firebase.auth().currentUser.uid
+      });
+    };
+
+    $scope.updaterBooking = function (bookingId, nyStatus) {
+      firebase.database().ref().child('bookinger').child(bookingId).update({
+        status : nyStatus
+      });
+    };
+
+/*
+    $scope.searchForArtist = function(searchText) {
+      FirebaseService.searchForArtist(searchText).then(function (data) {
+        $scope.searchResult = JSON.stringify(data, null, " ");
+      });
+    }
+*/
   }
 })();
