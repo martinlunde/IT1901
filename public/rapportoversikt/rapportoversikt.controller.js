dbfcc8b0af1d6429581107aa36afcf3180d2542e
martinlunde <martlun@stud.ntnu.no>
2016-10-04 13:10:24 +0200

    Opprettet rapportoversikt og ny-rapport html og controllere

---
+++ public/rapportoversikt/rapportoversikt.controller.js
@@ -0,0 +1,11 @@
+(function () {
+  "use strict";
+
+  angular.module("angularAuth")
+  .controller("rapportoversiktCtrl", rapportoversiktCtrl);
+
+  rapportoversiktCtrl.$inject = ["$scope", "FirebaseService"];
+  function rapportoversiktCtrl($scope, FirebaseService) {
+
+  }
+})();
